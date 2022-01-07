75574f451886263242f70d3fb835ae460a3383af
chmaurer <chmaurer@iu.edu>
2022-01-07 14:46:36 -0500

    No need to totally disable csrf.  Only ignore it for /lti

--- src/main/java/edu/iu/uits/lms/redirect/config/SecurityConfig.java
+++ src/main/java/edu/iu/uits/lms/redirect/config/SecurityConfig.java
@@ -57,8 +57,8 @@ public class SecurityConfig {
                   .authorizeRequests()
                   .antMatchers("/lti").permitAll();
 
-            //Need to disable csrf so that we can use POST via REST
-            http.csrf().disable();
+            //Need to disable csrf just for the /lti
+            http.csrf().ignoringAntMatchers("/lti");
 
             //Need to disable the frame options so we can embed this in another tool
             http.headers().frameOptions().disable();
